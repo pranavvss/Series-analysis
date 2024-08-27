bbcf036e7971cb399ba1b78c8834aa4a8075b510
pranavvss <kpranav3457@gmail.com>
2024-08-27 09:06:57 +0530

    character_network_addition

--- gradio_app.py
+++ gradio_app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 from theme_classifier import ThemeClassifier
-#from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
+from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
 #from text_classification import JutsuClassifier
 #from character_chatbot import CharacterChatBot
 #import os
@@ -32,7 +32,6 @@ def get_themes(theme_list_str,subtitles_path,save_path):
 
     return output_chart
 
-"""
 def get_character_network(subtitles_path,ner_path):
     ner = NamedEntityRecognizer()
     ner_df = ner.get_ners(subtitles_path,ner_path)
@@ -42,7 +41,7 @@ def get_character_network(subtitles_path,ner_path):
     html = character_network_generator.draw_network_graph(relationship_df)
 
     return html
-"""
+
 """
 def classify_text(text_classifcation_model,text_classifcation_data_path,text_to_classify):
     jutsu_classifier = JutsuClassifier(model_path = text_classifcation_model,
@@ -81,18 +80,18 @@ def main():
                         get_themes_button =gr.Button("Get Themes")
                         get_themes_button.click(get_themes, inputs=[theme_list,subtitles_path,save_path], outputs=[plot])
 
-        # Character Network Section
-        #with gr.Row():
-            #with gr.Column():
-                #gr.HTML("<h1>Character Network (NERs and Graphs)</h1>")
-                #with gr.Row():
-                    #with gr.Column():
-                        #network_html = gr.HTML()
-                    #with gr.Column():
-                        #subtitles_path = gr.Textbox(label="Subtutles or Script Path")
-                        #ner_path = gr.Textbox(label="NERs save path")
-                        #get_network_graph_button = gr.Button("Get Character Network")
-                        #get_network_graph_button.click(get_character_network, inputs=[subtitles_path,ner_path], outputs=[network_html])
+        #Character Network Section
+        with gr.Row():
+            with gr.Column():
+                gr.HTML("<h1>Character Network (NERs and Graphs)</h1>")
+                with gr.Row():
+                    with gr.Column():
+                        network_html = gr.HTML()
+                    with gr.Column():
+                        subtitles_path = gr.Textbox(label="Subtutles or Script Path")
+                        ner_path = gr.Textbox(label="NERs save path")
+                        get_network_graph_button = gr.Button("Get Character Network")
+                        get_network_graph_button.click(get_character_network, inputs=[subtitles_path,ner_path], outputs=[network_html])
 
         # Text Classification with LLMs
         #with gr.Row():
